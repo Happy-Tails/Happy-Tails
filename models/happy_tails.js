b8412d5b79bd0ddb56f3d78b96ba364ecb5c6594
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-05-12 14:06:40 -0400

    merge conflicts

--- models/happy_tails.js
+++ models/happy_tails.js
@@ -1,6 +1,47 @@
 module.exports = function(sequelize,dataTypes){
     var Trail = sequelize.define("Trail",{
         
-    })
-    return Trail
+    });
+    return Trail;
+};
+
+function showTrail() {
+    var url = "https://trailapi-trailapi.p.mashape.com/?lat=34.1&limit=25&lon=-105.2&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Denver&q[country_cont]=Australia&q[state_cont]=California&radius=25";
+
+    var city = $(this).attr("city");
+    var state = $(this).attr("statee");
+    var country = $(this).attr("country");
+    // (this is what I used to test api)
+    // var country = "United States";
+    // var state = "Ohio";
+    // var city = "Cleveland";
+
+    var queryURL = "https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=hiking&q[city_cont]=" + city + "&q[country_cont]=" + country + "&q[state_cont]=" + state;
+
+    console.log(queryURL);
+ 
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {
+            "X-Mashape-Key": "mdooQIOQIVmshTWRLBxh7vmwiYS3p1TjHYZjsnha3vcYMIF7Tl"
+        }
+    }).then(function (response) {
+        console.log(response);
+    });
+
+    // // Need a div to hold the trial
+    //   var trailDiv = $("<div class='trail'>");
+
+    //   // Storing the trail rating
+    //   var rating = response.rating;
+
+    //   // Creating an element to have the rating displayed
+    //   var trailRating = $("<p>").text("Rating: " + rating);
+
+    //   // Displaying the rating
+    //   trailDiv.append(trailRating);
+    // });
 }
+
+showTrail();
