8a9fef9175410e315c4b8cd76776e7573fd97e28
Matt Pedicini <mattpedicini@Matts-MacBook-Air.local>
2018-05-22 18:04:09 -0400

    transitioned CRUD to sequelize

--- controllers/crud-controller.js
+++ controllers/crud-controller.js
@@ -47,75 +47,70 @@ crudRouter.get("/user/login/:user/:password",function(req,res){
 //CRUD
 
 //caling our createTrail function
-createTrail();
+
 
 function createTrail() {
     console.log("Adding your new trail...\n");
-    var query = connection.query(
-    //Need to put in correct table name
-      "INSERT INTO tableName SET ?",
-      {
+    db.Trails.create({
         name: "trail name",
         description: "trail description",
         length: "trail length"
-      },
-      function(err, res) {
+    }).then(function(err, res) {
         console.log(res.affectedRows + " trail added!\n");
         // Call updateTrail AFTER the INSERT completes
         updateTrail();
-      }
-    );
+      });
+    //Need to put in correct table name
+      
   
     // logs the actual query being run
-    console.log(query.sql);
+    //console.log(query.sql);
   }
   
   function updateTrail() {
     console.log("Updating your saved trails...\n");
-    var query = connection.query(
-    //Need to put in the correct table name
-      "UPDATE tableName SET ? WHERE ?",
-      [
-          {},
-          {}
-      ],
-      function(err, res) {
+    db.Trails.update({
+
+    }).then(function(err, res) {
         console.log(res.affectedRows + " trails updated!\n");
         // Call deleteTrail AFTER the UPDATE completes
         deleteTrail();
-      }
-    );
+      });
+    //Need to put in the correct table name
+      
+      
+    
   
     // logs the actual query being run
-    console.log(query.sql);
+    //console.log(query.sql);
   }
   
   function deleteTrail() {
     console.log("Deleting selected trail...\n");
-    connection.query(
-    //Need to add correct tableName
-      "DELETE FROM tableName WHERE ?",
-      {
-        name: "name of trail you want to delete"
-      },
-      function(err, res) {
+    db.Trails.destroy({
+      where:{
+        id: req.params.id
+      }
+    }).then(function(err, res) {
         console.log(res.affectedRows + " trail(s) deleted!\n");
         // Call readTrails AFTER the DELETE completes
         readTrails();
-      }
-    );
+      });
+    //Need to add correct tableName
+      
+    
   }
   
   function readTrails() {
     console.log("Selecting all trails...\n");
-    connection.query("SELECT * FROM tableName", function(err, res) {
+    db.Trails.findAll().then(function(res,err){
       if (err) throw err;
       // Log all results of the SELECT statement
       console.log(res);
       connection.end();
     });
   }
-  
+createTrail();  
 // ----------------------------------------------------
 
 // Export routes
