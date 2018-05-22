7ec1802bc865b6fc4526fea3565f449bafc689fe
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-05-22 06:18:43 -0400

    added notes to controller and CRUD function

--- controllers/crud-controller.js
+++ controllers/crud-controller.js
@@ -5,9 +5,76 @@ var models = require('../models'); // Pulls out the Models
 
 
 // POST/API Routes for Database changes
+
+//CRUD
+function createProduct() {
+    console.log("Adding your new trail...\n");
+    var query = connection.query(
+    //Need to put in correct table name
+      "INSERT INTO tableName SET ?",
+      {
+        name: "trail name",
+        description: "trail description",
+        length: "trail length"
+      },
+      function(err, res) {
+        console.log(res.affectedRows + " trail added!\n");
+        // Call updateTrail AFTER the INSERT completes
+        updateTrail();
+      }
+    );
+  
+    // logs the actual query being run
+    console.log(query.sql);
+  }
+  
+  function updateTrail() {
+    console.log("Updating your saved trails...\n");
+    var query = connection.query(
+    //Need to put in the correct table name
+      "UPDATE tableName SET ? WHERE ?",
+      [
+          {},
+          {}
+      ],
+      function(err, res) {
+        console.log(res.affectedRows + " trails updated!\n");
+        // Call deleteTrail AFTER the UPDATE completes
+        deleteTrail();
+      }
+    );
+  
+    // logs the actual query being run
+    console.log(query.sql);
+  }
+  
+  function deleteTrail() {
+    console.log("Deleting selected trail...\n");
+    connection.query(
+    //Need to add correct tableName
+      "DELETE FROM tableName WHERE ?",
+      {
+        name: "name of trail you want to delete"
+      },
+      function(err, res) {
+        console.log(res.affectedRows + " trail(s) deleted!\n");
+        // Call readTrails AFTER the DELETE completes
+        readTrails();
+      }
+    );
+  }
+  
+  function readTrails() {
+    console.log("Selecting all trails...\n");
+    connection.query("SELECT * FROM tableName", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
+  
 // ----------------------------------------------------
-
-
 
 // Export routes
 module.exports = crudRouter;
--- controllers/happy_tails_controllers.js
+++ controllers/happy_tails_controllers.js
@@ -3,23 +3,27 @@ var express = require("express");
 
 var router = express.Router();
 var path = require("path");
+
+//Here are our routes and the logic needed to make them happen
 router.get("/",function(req,res){
     res.sendFile(path.join(__dirname, "../views/layouts/index.html"));
 });
 router.get("/sign_up",function(req,res){
-    res.sendfile(path.join(__dirname, "../views/layouts/signup.html"))
+    res.sendfile(path.join(__dirname, "../views/layouts/signup.html"));
 });
 router.get("/api/trails",function(req,res){
     db.Trail.findAll().then(function(data){
         res.json(data);
-    })
-})
+    });
+});
 
 router.post("/api/trails",function(req,res){
     db.Trail.create({
 
-    })
-})
+    });
+});
+
+//GET route for one user's email and password
 router.get("/api/login",function(req,res){  
     db.Login.findOne({
         where:{
@@ -31,9 +35,11 @@ router.get("/api/login",function(req,res){
       }).catch((err)=>{
         res.status(500).json({
           error: "Email or Password is incorrect."
-        })
-      })
-})
+        });
+      });
+});
+
+//POST route for login information 
 router.post("/api/login",function(req,res){
     db.Login.create({
         email: req.body.email,
@@ -46,7 +52,7 @@ router.post("/api/login",function(req,res){
     }).catch((err)=>{
         res.status(500).json({
             error:err.message
-        })
-    })
-})
+        });
+    });
+});
 module.exports = router;
