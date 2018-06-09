2c80b9e632360b992e8a8751187f69690ec8789f
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-09 04:21:39 -0400

    added JawsDB for Heroku

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,25 @@
 var Sequelize = require("sequelize");
 
 
-   var sequelize = new Sequelize ("sequelize_library","root","root",{  //the first argument should be my database if there is an error
-       host: "localhost",
-       dialect:"mysql",
-       pool:{
-           max:5,
-           min:0,
-           idle:10000
-       }
-   });
+var sequelize = new Sequelize("sequelize_library", "root", "root", {  //the first argument should be my database if there is an error
+    //need to host DB, see JawsDB exercise
+    //checkout wiht cc on Heroku 
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    // jawsDB
+    jawsDB: {
+        port: 3306,
+        host: "<host name>",
+        user: "<name of user>",
+        password: "<password>",
+        database: "<name of database>"
+    }
+});
 
-  module.exports = sequelize;
+
+module.exports = sequelize;
