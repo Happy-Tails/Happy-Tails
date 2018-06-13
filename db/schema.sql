db850544748b25ad10a41847c56e114c87bacfaf
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-12 22:53:53 -0400

    udpated myTrail Card

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ USE happytails;
 CREATE TABLE trails(
   item_id INT AUTO_INCREMENT NOT NULL,
   trailName VARCHAR(45) NOT NULL,
-  trailDescription VARCHAR(1000) NOT NULL,
+  trailDescription VARCHAR(10000) NOT NULL,
   trailLength INT NOT NULL,
   primary key(item_id)
 );
